d71d4432501377dc217aee01b1f9f47456114117
Alex Naidis <alex.naidis@linux.com>
2017-04-09 01:29:27 +0200

    init: Weaken property override security for the init extension
    Sometimes we need to override ro.* properties by using our vendor init
    extension.
    
    Previously there was a security check which was blocking that.
    To resolve the issue, we need to weaken the security check during the
    execution of our vendor init extension.
    
    This is safe because the vendor init extension gets executed as part of init
    construction and it is considered a trusted system component.
    
    Change-Id: Ia7d60686968695f1fb43be4ed58770ce10da88c5
    
    Former-commit-id: 91d12168b9500a44211a7d5a092e0ebcfed48d4d

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -108,6 +108,8 @@ struct PropertyAuditData {
     const char* name;
 };
 
+static bool weaken_prop_override_security = false;
+
 static int PropertyAuditCallback(void* data, security_class_t /*cls*/, char* buf, size_t len) {
     auto* d = reinterpret_cast<PropertyAuditData*>(data);
 
@@ -178,8 +180,8 @@ static uint32_t PropertySet(const std::string& name, const std::string& value, s
 
     prop_info* pi = (prop_info*) __system_property_find(name.c_str());
     if (pi != nullptr) {
-        // ro.* properties are actually "write-once".
-        if (StartsWith(name, "ro.")) {
+        // ro.* properties are actually "write-once", unless the system decides to
+        if (StartsWith(name, "ro.") && !weaken_prop_override_security) {
             *error = "Read-only property was already set";
             return PROP_ERROR_READ_ONLY_PROPERTY;
         }
@@ -913,11 +915,17 @@ void PropertyLoadBootDefaults() {
         }
     }
 
+    // Weaken property override security during execution of the vendor init extension
+    weaken_prop_override_security = true;
+
     // Update with vendor-specific property runtime overrides
     vendor_load_properties();
 
     property_initialize_ro_product_props();
     property_derive_build_fingerprint();
+
+    // Restore the normal property override security after init extension is executed
+    weaken_prop_override_security = false;
 
     if (android::base::GetBoolProperty("ro.persistent_properties.ready", false)) {
         update_sys_usb_config();
