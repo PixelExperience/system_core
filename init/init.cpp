ff07e0a831647ce4aa7aa2060e2cf32adee875ba
LuK1337 <priv.luk@gmail.com>
2017-05-22 18:46:00 -0500

    init: I hate safety net
    Change-Id: I22692f58f4f9b73fe66ef87fc0f9558937adbf02

--- init/init.cpp
+++ init/init.cpp
@@ -374,7 +374,11 @@ static void import_kernel_nv(const std::string& key, const std::string& value, b
         return;
     }
 
-    if (key == "qemu") {
+    if (key == "androidboot.verifiedbootstate" ||
+            key == "androidboot.veritymode" ||
+            key == "androidboot.warranty_bit") {
+        return;
+    } else if (key == "qemu") {
         strlcpy(qemu, value.c_str(), sizeof(qemu));
     } else if (android::base::StartsWith(key, "androidboot.")) {
         property_set("ro.boot." + key.substr(12), value);
@@ -382,15 +386,7 @@ static void import_kernel_nv(const std::string& key, const std::string& value, b
 }
 
 static void export_oem_lock_status() {
-    if (!android::base::GetBoolProperty("ro.oem_unlock_supported", false)) {
-        return;
-    }
-    import_kernel_cmdline(
-            false, [](const std::string& key, const std::string& value, bool in_qemu) {
-                if (key == "androidboot.verifiedbootstate") {
-                    property_set("ro.boot.flash.locked", value == "orange" ? "0" : "1");
-                }
-            });
+    property_set("ro.boot.flash.locked", "1");
 }
 
 static void export_kernel_boot_props() {
