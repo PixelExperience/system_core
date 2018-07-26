500f155918bd68d220ef235a9a3a67981564bdde
ShevT <shev_t@mail.ru>
2018-07-26 11:47:47 +0300

    healthd: BatteryMonitor: Fix compiler warning
    system/core/healthd/BatteryMonitor.cpp:248:19: note: initialize the variable 'i' to silence this warning
        unsigned int i;
                      ^
                       = 0
    1 warning generated.
    
    Change-Id: Ib2fa0061d657fd93871ca8a9056d173d5b7ab58d
    
    Former-commit-id: d37510641066136d9f3659ec5818c503ea16f74d

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -240,7 +240,6 @@ bool BatteryMonitor::update(void) {
     if (readFromFile(mHealthdConfig->batteryTechnologyPath, &buf) > 0)
         props.batteryTechnology = String8(buf.c_str());
 
-    unsigned int i;
     double MaxPower = 0;
 
     // Rescan for the available charger types
@@ -277,7 +276,7 @@ bool BatteryMonitor::update(void) {
         }
     }
 
-    for (i = 0; i < mChargerNames.size(); i++) {
+    for (size_t i = 0; i < mChargerNames.size(); i++) {
         String8 path;
         path.appendFormat("%s/%s/online", POWER_SUPPLY_SYSFS_PATH,
                           mChargerNames[i].string());
