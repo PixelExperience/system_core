c25851d0da41fced490c50b96d1fcdd19e6fa417
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 20:33:21 -0300

    init: Only set safetynet props if not eng build
    Change-Id: Ic07539b4a7a97316720defd000425d1b6d15fd67
    
    Former-commit-id: 9d4ca9403943feecd6f902e69d581aad3ee84839
    Change-Id: Ic34d95c23afd8caf95c7b2a2517650dbf116fdde
    
    Former-commit-id: 1b99c0d0cc89f113d35eb065e435f61b51408b12

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -840,16 +840,20 @@ static const char *snet_prop_value[] = {
 };
 
 static void workaround_snet_properties() {
+    std::string build_type = android::base::GetProperty("ro.build.type", "");
+
     // Weaken property override security to set safetynet props
     weaken_prop_override_security = true;
 
 	std::string error;
-	LOG(INFO) << "snet: Hiding sensitive props";
 
-	// Hide all sensitive props
-	for (int i = 0; snet_prop_key[i]; ++i) {
-		PropertySet(snet_prop_key[i], snet_prop_value[i], &error);
-	}
+	// Hide all sensitive props if not eng build
+    if (build_type != "eng") {
+	    LOG(INFO) << "snet: Hiding sensitive props";
+	    for (int i = 0; snet_prop_key[i]; ++i) {
+            PropertySet(snet_prop_key[i], snet_prop_value[i], &error);
+	    }
+    }
 
     // Restore the normal property override security after safetynet props have been set
     weaken_prop_override_security = false;
