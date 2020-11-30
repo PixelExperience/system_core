4dedf834ab2eced0b27913b9d594f0814e2caac8
PixelExperience Automation <github@pixelexperience.org>
2020-11-30 23:36:06 +0000

    dnm - test - revert safetynet commits Revert "init: Only set safetynet props if not eng build"
    This reverts commit a70056dab29867c1a69c156b75e57a94057f9854.
    
    Revert "init: Allow forcing custom build fingerprint [2/2]"
    
    This reverts commit b6d329c719e01c218ff097511508eeab093e7283.
    
    Revert "init: Weaken property override security only when spoofing safetynet"
    
    This reverts commit 72e44946d6e3ef7a009d80f251126fd0bf2a3959.
    
    Revert "init: add vendor.*  keys to spoof safetynet"
    
    This reverts commit 99805cc41df7d3406a9e320739ff111349ff639c.
    
    Revert "init: workaround SafetyNet check"
    
    This reverts commit b6872d7d580f2abcae6398a329d9409309c1c1ff.
    
    Revert "init: Weaken property override security for the init extension"
    
    This reverts commit 13b87c14dc0397a33bbe38898daedcc0eb9e3d2e.
    
    Change-Id: I362ae904b7985d20751baad77ddd5b31ca4859ca

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -108,8 +108,6 @@ struct PropertyAuditData {
     const char* name;
 };
 
-static bool weaken_prop_override_security = false;
-
 static int PropertyAuditCallback(void* data, security_class_t /*cls*/, char* buf, size_t len) {
     auto* d = reinterpret_cast<PropertyAuditData*>(data);
 
@@ -180,8 +178,8 @@ static uint32_t PropertySet(const std::string& name, const std::string& value, s
 
     prop_info* pi = (prop_info*) __system_property_find(name.c_str());
     if (pi != nullptr) {
-        // ro.* properties are actually "write-once", unless the system decides to
-        if (StartsWith(name, "ro.") && !weaken_prop_override_security) {
+        // ro.* properties are actually "write-once".
+        if (StartsWith(name, "ro.")) {
             *error = "Read-only property was already set";
             return PROP_ERROR_READ_ONLY_PROPERTY;
         }
@@ -773,87 +771,6 @@ static void load_override_properties() {
     }
 }
 
-static const char *snet_prop_key[] = {
-	"ro.boot.vbmeta.device_state",
-	"ro.boot.verifiedbootstate",
-	"ro.boot.flash.locked",
-	"ro.boot.selinux",
-	"ro.boot.veritymode",
-	"ro.boot.warranty_bit",
-	"ro.warranty_bit",
-	"ro.debuggable",
-	"ro.secure",
-	"ro.build.type",
-	"ro.build.keys",
-	"ro.build.tags",
-	"ro.system.build.tags",
-	"ro.vendor.boot.warranty_bit",
-	"ro.vendor.warranty_bit",
-	"vendor.boot.vbmeta.device_state",
-	"vendor.boot.verifiedbootstate",
-	NULL
-};
-
-static const char *snet_prop_value[] = {
-	"locked", // ro.boot.vbmeta.device_state
-	"green", // ro.boot.verifiedbootstate
-	"1", // ro.boot.flash.locked
-	"enforcing", // ro.boot.selinux
-	"enforcing", // ro.boot.veritymode
-	"0", // ro.boot.warranty_bit
-	"0", // ro.warranty_bit
-	"0", // ro.debuggable
-	"1", // ro.secure
-	"user", // ro.build.type
-	"release-keys", // ro.build.keys
-	"release-keys", // ro.build.tags
-	"release-keys", // ro.system.build.tags
-	"0", // ro.vendor.boot.warranty_bit
-	"0", // ro.vendor.warranty_bit
-	"locked", // vendor.boot.vbmeta.device_state
-	"green", // vendor.boot.verifiedbootstate
-	NULL
-};
-
-#ifdef TARGET_FORCE_BUILD_FINGERPRINT
-static const char *build_fingerprint_key[] = {
-    "ro.build.fingerprint",
-	"ro.system_ext.build.fingerprint",
-	"ro.vendor.build.fingerprint",
-	"ro.bootimage.build.fingerprint",
-	"ro.odm.build.fingerprint",
-	"ro.product.build.fingerprint",
-	"ro.system.build.fingerprint",
-	NULL
-};
-#endif
-
-static void workaround_snet_properties() {
-    std::string build_type = android::base::GetProperty("ro.build.type", "");
-
-    // Weaken property override security to set safetynet props
-    weaken_prop_override_security = true;
-
-	std::string error;
-
-	// Hide all sensitive props if not eng build
-    if (build_type != "eng") {
-	    LOG(INFO) << "snet: Hiding sensitive props";
-	    for (int i = 0; snet_prop_key[i]; ++i) {
-            PropertySet(snet_prop_key[i], snet_prop_value[i], &error);
-	    }
-    }
-
-    #ifdef TARGET_FORCE_BUILD_FINGERPRINT
-        for (int i = 0; build_fingerprint_key[i]; ++i) {
-            PropertySet(build_fingerprint_key[i], TARGET_FORCE_BUILD_FINGERPRINT, &error);
-        }
-    #endif
-
-    // Restore the normal property override security after safetynet props have been set
-    weaken_prop_override_security = false;
-}
-
 // If the ro.product.[brand|device|manufacturer|model|name] properties have not been explicitly
 // set, derive them from ro.product.${partition}.* properties
 static void property_initialize_ro_product_props() {
@@ -923,7 +840,6 @@ static void property_initialize_ro_product_props() {
     }
 }
 
-#ifndef TARGET_FORCE_BUILD_FINGERPRINT
 // If the ro.build.fingerprint property has not been set, derive it from constituent pieces
 static void property_derive_build_fingerprint() {
     std::string build_fingerprint = GetProperty("ro.build.fingerprint", "");
@@ -957,7 +873,6 @@ static void property_derive_build_fingerprint() {
                    << ")";
     }
 }
-#endif
 
 void PropertyLoadBootDefaults() {
     // TODO(b/117892318): merge prop.default and build.prop files into one
@@ -1002,17 +917,11 @@ void PropertyLoadBootDefaults() {
     vendor_load_properties();
 
     property_initialize_ro_product_props();
-
-#ifndef TARGET_FORCE_BUILD_FINGERPRINT
     property_derive_build_fingerprint();
-#endif
 
     if (android::base::GetBoolProperty("ro.persistent_properties.ready", false)) {
         update_sys_usb_config();
     }
-
-    // Workaround SafetyNet
-    workaround_snet_properties();
 }
 
 bool LoadPropertyInfoFromFile(const std::string& filename,
